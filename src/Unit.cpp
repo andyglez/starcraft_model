458d1f2ee2b7bc5f6a035f9f7b6475d8c60c59bd
Andy Gonzalez <aglez9538@gmail.com>
2018-06-14 01:03:06 -0400

    Added Unit.cpp base class

---
+++ src/Unit.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Andy on 14/06/2018.
+//
+
+#include <iostream>
+using namespace std;
+
+class Unit{
+    int _life, _attack, _defense;
+    string _name;
+
+protected:
+    Unit(int life, int attack, int defense, string&& name) :
+            _life(life), _attack(attack), _defense(defense), _name(name){}
+
+public:
+    void print(){
+        cout << _name << endl;
+    }
+};
+
